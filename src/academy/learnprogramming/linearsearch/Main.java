8d43d37bc53b4510839a0a762875f7ff1581c196
Tootin0730 <yisan1004@gmail.com>
2023-06-20 19:36:26 +0900

    Binary Search implementation

--- src/academy/learnprogramming/linearsearch/Main.java
+++ src/academy/learnprogramming/linearsearch/Main.java
@@ -3,22 +3,50 @@ package academy.learnprogramming.linearsearch;
 public class Main {
 
     public static void main(String[] args) {
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
         System.out.println(iterativeBinarySearch(intArray, -15));
-        System.out.println(iterativeBinarySearch(intArray, 1));
+        System.out.println(iterativeBinarySearch(intArray, 35));
         System.out.println(iterativeBinarySearch(intArray, 8888));
-        System.out.println(iterativeBinarySearch(intArray, -22));
+        System.out.println(iterativeBinarySearch(intArray, 1));
 
     }
 
     public static int iterativeBinarySearch(int[] input, int value) {
-        for(int i = 0; i < input.length; i++){
-            if (input[i] == value){
-                return i;
+        int start = 0;
+        int end = input.length;
+
+        while (start < end) {
+            int midpoint = (start + end) / 2;
+            System.out.println("midpoint = " + midpoint);
+            if (input[midpoint] == value) {
+                return midpoint;
+            } else if (input[midpoint] < value) {
+                start = midpoint + 1;
+            } else {
+                end = midpoint;
             }
         }
-
         return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] input, int value) {
+        return recursiveBinarySearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveBinarySearch(int[] input, int start, int end, int value) {
+        if (start >= end){
+            return -1;
+        }
+        int midpoint = (start + end) / 2;
+        System.out.println("midpoint = " + midpoint);
+
+        if (input[midpoint] == value) {
+            return midpoint;
+        }else if(input[midpoint] < value) {
+            return recursiveBinarySearch(input, midpoint + 1, end, value);
+        }else{
+            return recursiveBinarySearch(input, start, midpoint, value);
+        }
     }
 }
